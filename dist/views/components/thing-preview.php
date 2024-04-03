dff663589f6df6ebece5d1c1198ed89deabc3256
Steve Copley <cpy@dt.waimea.school.nz>
2024-04-04 11:09:08 +1300

    .htaccess tweaks and removal of redundant code

--- dist/views/components/thing-preview.php
+++ dist/views/components/thing-preview.php
@@ -2,10 +2,6 @@
 
 <h2>Thing <?= $id ?></h2>
 
-<img src="<?= SITE_BASE ?>/images/thing<?= $id ?>.jpg">
-
 <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 </p>
-
-<p><a href="<?= SITE_BASE ?>/thing/<?= $id ?>">Find out more...</a></p>
